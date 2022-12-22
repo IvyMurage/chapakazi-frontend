ad99ec0ddbed045e2828f04f05b75f1c89145be6
Noela-Murugi <murugin254@gmail.com>
2022-12-22 19:12:33 +0300

    return form

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -33,6 +33,122 @@ function SignUpForm() {
   }
 
 
+  return (
+    <div className="form">
+      <div className="signup-container">
+        <div className="right-container" onSubmit={handleFormSubmit}>
+            <header>
+                <div className="set">
+                </div>
+                <div className="set">
+
+                <div className="customer-name">
+                    <label htmlFor="customer-name">Name</label>
+                    <input
+                    id="customer-name"
+                    //   placeholder="Name..."
+                    name="name"
+                    type="text"
+                    value={formData.name}
+                    onChange={handleInputChange}
+                    required
+                    ></input>
+                </div>
+
+                <div className="customer-location">
+                    <label htmlFor="customer-venue">Location</label>
+                    <input
+                    id="customer-venue"
+                    //   placeholder="Location..."
+                    name="location"
+                    type="text"
+                    value={formData.location}
+                    onChange={handleInputChange}
+                    required
+                    ></input>
+                </div>
+                </div>
+                <div className="set">
+                <div className="customer-name">
+                    <label htmlFor="customer-email">Email</label>
+                    <input
+                    id="customer-name"
+                    //   placeholder="Email..."
+                    name="email"
+                    type="email"
+                    value={formData.email}
+                    onChange={handleInputChange}
+                    required
+                    ></input>
+                </div>
+
+                <div className="customer-name">
+                    <label htmlFor="customer-password">Password</label>
+                    <input
+                    id="customer-name"
+                    //   placeholder="Password..."
+                    name="password"
+                    type="password"
+                    value={formData.password}
+                    onChange={handleInputChange}
+                    required
+                    ></input>
+                </div>
+                </div>
+
+                <div className="set">
+
+                    <div className="customer-name">
+                        <label htmlFor="customer-image">Image</label>
+                        <input
+                        id="customer-name"
+                        //   placeholder="Image..."
+                        name="image"
+                        type="text"
+                        value={formData.image}
+                        onChange={handleInputChange}
+                        required
+                        ></input>
+                    </div>
+
+                    <div className="customer-location">
+                    <label htmlFor="customer-id">Admin ID</label>
+                    <input
+                    id="customer-ID"
+                    //   placeholder="ID..."
+                    name="id"
+                    type="number"
+                    value={formData.id}
+                    onChange={handleInputChange}
+                    required
+                    ></input>
+                </div>
+                </div>
+
+                <div className="customer-description">
+                <label htmlFor="customer-weight-0-25">Description</label>
+                <div className="description-container">
+                    <input
+                    type="text"
+                    name="description"
+                    value={formData.description}
+                    onChange={handleInputChange}
+                    required
+                        />
+                </div>
+                </div>
+            </header>
+            <footer>
+                <div className="set">
+                <button id="next" type="submit" onClick={handleFormSubmit}>
+                    Submit
+                </button>
+                </div>
+            </footer>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default SignUpForm;
