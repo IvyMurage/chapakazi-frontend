3694a0955952a830e94d464c6ea4018a171c1c10
Murage-Ivy <ivy.murage@student.moringaschool.com>
2022-12-23 14:16:21 +0300

    Add the registrationContainer and LandingTop Container in the LandingPage Component

--- src/Components/landingPage/LandingPage.js
+++ src/Components/landingPage/LandingPage.js
@@ -1,27 +1,12 @@
 import React from "react";
-import "./LandingPage.css";
+import LandingTop from "./landingTopContainer/LandingTop";
+import RegistrationCard from "./registrationContainer/RegistrationCard";
 
 function LandingPage() {
   return (
     <div className="landing-page">
-      <div className="top-container">
-        <div id="image-container">
-          <img
-            src="./images/wepik-photo-mode-20221114-16341.png"
-            alt="plumber with arrms crossed"
-          />
-        </div>
-        <div id="start-section">
-          <h2>
-            Chapa <span id="kazi">Kazi</span>
-            <br />
-            Ajira Mkononi
-          </h2>
-          <button id="get-started">Get Started</button>
-        </div>
-      </div>
-
-      <div className="bottom-container"></div>
+      <LandingTop />
+      <RegistrationCard />
     </div>
   );
 }
