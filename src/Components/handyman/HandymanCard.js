efc154f782994dc9d8747903826566abf85fc7d5
Rajab <rkarume@gmail.com>
2023-01-09 13:37:41 +0300

    styling the card

--- src/Components/handyman/HandymanCard.js
+++ src/Components/handyman/HandymanCard.js
@@ -5,20 +5,24 @@ import "./Handyman.css";
 function HandymanCard(){
 
     return(
-        <div>
+        <div className="handymancard">
             <div className="image">
-                <img src="https://images.unsplash.com/photo-1621905251189-08b45d6a269e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTd8fGhhbmR5bWFufGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60" />
-                <h1>John Doe</h1>
-                <h2>Electrician</h2>
+                <img className="image" src="https://images.unsplash.com/photo-1621905251189-08b45d6a269e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTd8fGhhbmR5bWFufGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60" />
+                <h1 className="name">John Doe</h1>
+                <h2 className="occupation">Electrician</h2>
             </div>
             <div className="info">
-                <span>Nairobi</span>
-                <span>Ratings: 4.8</span>
-                <button>more</button>
-            </div>
-            <div className="description">
-                <h2>Description</h2>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis leo nisi, cursus et aliquam et, porta a erat. Praesent sollicitudin bibendum erat, in efficitur sem volutpat vel. Nullam vel maximus eros. Vivamus pellentesque rhoncus dui at feugiat. Fusce ut dolor venenatis, iaculis lectus efficitur, pulvinar massa.</p>
+                <div className="info-top">
+                    <span className="location">Nairobi</span>
+                    <span className="rating">Ratings: 4.8</span>
+                    <span className="button-span">
+                        <button className="button">more</button>
+                    </span>
+                </div>
+                <div className="info-bottom">
+                    <h2 className="description-heading">Description</h2>
+                    <p className="paragraph">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis leo nisi, cursus et aliquam et, porta a erat. Praesent sollicitudin bibendum erat, in efficitur sem volutpat vel. Nullam vel maximus eros. Vivamus pellentesque rhoncus dui at feugiat. Fusce ut dolor venenatis, iaculis lectus efficitur, pulvinar massa.</p>
+                </div>
             </div>
         </div>
     )
