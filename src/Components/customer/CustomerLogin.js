bf5967237d9f8489a999e7b21a06e4de1d16479c
Rlynette <lynette.ronnoh@student.moringaschool.com>
2023-01-10 12:26:16 +0300

    implement dispatch function

--- src/Components/customer/CustomerLogin.js
+++ src/Components/customer/CustomerLogin.js
@@ -3,22 +3,28 @@ import { useDispatch } from 'react-redux';
 import "./CustomerLogin.css"
 
 const CustomerLogin = () => {
-// const [name, setName] = useState(" ");
-const [email, setEmail] = useState(" ");
-const [password, setPassword] = useState(" ");
+// const [name, setName] = useState("");
+const [email, setEmail] = useState("");
+const [password, setPassword] = useState("");
 
+  const dispatch = useDispatch();
 
+  const handleFormSubmit = (e) =>{
+    e.preventDefault();
+
+   dispatch(
+    CustomerLogin({
+    email: email,
+    password: password,
+    loggedIn: true,
+     })
+   );
+  };
 
   return (
     <div className="login">
       <form className="login__form">
-        {/* <h1>Login here</h1>
-        <input
-          type="name"
-          placeholder="Name"
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-        /> */}
+        <h1>Login</h1>
         <input
           type="email"
           placeholder="Email"
@@ -31,9 +37,12 @@ const [password, setPassword] = useState(" ");
           value={password}
           onChange={(e) => setPassword(e.target.value)}
         />
-        <button type='login' className='login__btn'>Login</button>
-        <h1>
-        Do not have an account <span className='signup'>Sign Up</span>  </h1> 
+        <div className="customer-submit-btn">
+          <button onClick={(e) => handleFormSubmit(e)}>
+            Login
+            </button>
+        </div>
+        <h2> Do not have an account ? Sign Up </h2>
       </form>
     </div>
   );
