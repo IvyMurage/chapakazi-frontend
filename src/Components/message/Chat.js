8f560ad837d533a46c23e8d034cba3506920dcca
Rajab <rkarume@gmail.com>
2023-01-11 01:13:52 +0300

    created message side bar

--- src/Components/message/Chat.js
+++ src/Components/message/Chat.js
@@ -4,9 +4,10 @@ import "./Message.css"
 function Chat(){
     return(
         <div className="chat-user">
-            <img className="user-image" src="https://img.a.transfermarkt.technology/portrait/big/8198-1668503200.jpg?lm=1" />
+            <img className="user-image" src="https://icdn.caughtoffside.com/wp-content/uploads/2019/10/ronaldo-insta.jpg" />
             <div className="users">
-                <span>Christiano Ronaldo</span>
+                <span className="user-name">Christiano Ronaldo</span>
+                <p className="text-preview">Hello John Doe. I'm looking for a plumber</p>
             </div>
         </div>
     )
