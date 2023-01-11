41ab050fed04fa9977f81cf1e90de479912d2a33
Rlynette <lynette.ronnoh@student.moringaschool.com>
2023-01-11 11:21:24 +0300

    edit customer.css

--- src/Components/customer/CustomerLogin.js
+++ src/Components/customer/CustomerLogin.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
-
+import "./Customer.css";
 
 const CustomerLogin = () => {
 // const [name, setName] = useState("");
