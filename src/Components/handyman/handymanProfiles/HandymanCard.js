b684eb501f7449f234ac91f5d1e0848eea4766a2
Murage-Ivy <ivy.murage@student.moringaschool.com>
2023-01-14 11:34:26 +0300

    Display handyman details on indivual cards

--- src/Components/handyman/handymanProfiles/HandymanCard.js
+++ src/Components/handyman/handymanProfiles/HandymanCard.js
@@ -1,7 +1,14 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import "../Handyman.css";
 
 function HandymanCard({ profile }) {
+  const navigate = useNavigate();
+  function handleProfile(profileId) {
+    localStorage.setItem("profileId", profile.id);
+    navigate(`/profilepage`);
+  }
+
   return (
     <div className="container">
       <div className="handymancard">
@@ -15,18 +22,20 @@ function HandymanCard({ profile }) {
           <div className="info-top">
             <span className="location"> {profile.location} </span>
             <span className="rating"> {profile.rating} </span>
-            <span className="button-span">more </span>
+            <span
+              className="button-span"
+              onClick={() => handleProfile(profile.id)}
+            >
+              more{" "}
+            </span>
           </div>
 
           <div className="info-bottom">
             <h2 className="description-heading"> Description </h2>
             <p className="paragraph"> {profile.description} </p>
           </div>
-
         </div>
-
       </div>
-
     </div>
   );
 }
